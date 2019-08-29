b612cf59d4a1bad1434435d8c654cff34b69694d
victoria-t <54410079+victoria-t@users.noreply.github.com>
2019-08-29 11:30:50 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Victoria Titsworth
+ * Date: 2019/27/08
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Victoria Titsworth!\n");
+
+  return 0;
+}
